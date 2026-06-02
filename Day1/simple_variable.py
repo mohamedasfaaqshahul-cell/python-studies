c6627edd8e8d739a842e83b98a8c62173672927c
mohamedasfaaqshahul-cell <mohamedasfaaq.shahul@gmail.com>
2026-06-02 21:16:30 +0530

    Create simple_variable

---
+++ Day1/simple_variable.py
@@ -0,0 +1,12 @@
+# Get details from user
+
+name = input("Enter Your Name: ")
+gmail_id = input("Enter Gmail ID: ")
+recovery_email = input("Enter Recovery Email: ")
+
+# Print details
+
+print("\n--- Gmail Registration Details ---")
+print("Name:", name)
+print("Gmail ID:", gmail_id)
+print("Recovery Email:", recovery_email)
