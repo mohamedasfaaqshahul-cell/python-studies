75790c7562ffa34543dfcf3d6cee627cc3398f6d
mohamedasfaaqshahul-cell <mohamedasfaaq.shahul@gmail.com>
2026-06-03 20:10:08 +0530

    Create updating_removing.py

---
+++ Day2/updating_removing.py
@@ -0,0 +1,11 @@
+UPDATING:
+flights = ["IndiGo", "Air India", "SpiceJet"]
+flights[1] = "Vistara"
+print(flights)
+
+REMOVING:
+flights = ["IndiGo", "Air India", "SpiceJet"]
+flights.remove("Air India")
+print(flights)
+flights.pop(0)
+print(flights)
