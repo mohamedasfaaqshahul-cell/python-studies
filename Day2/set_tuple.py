ee12952eb5b0c80a8f3dda624a59b1c9b59bad12
mohamedasfaaqshahul-cell <mohamedasfaaq.shahul@gmail.com>
2026-06-03 19:47:24 +0530

    Create set_tuple.py

---
+++ Day2/set_tuple.py
@@ -0,0 +1,7 @@
+SET EXAMPLE:
+passengers = {"ASSU", "MOHAMED", "SHAHUL"}
+print(passengers)
+
+TUPLE EXAMPLE:
+flight_routes = ("Coimbatore to Delhi", "Coimbatore to Mumbai", "Coimbatore to Bangalore")
+print(flight_routes[0])
