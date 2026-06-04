600c8adf4344082b8d89a5457337787a8983588a
mohamedasfaaqshahul-cell <mohamedasfaaq.shahul@gmail.com>
2026-06-04 16:38:12 +0530

    Create dictionary_read.py

---
+++ Day3/dictionary_read.py
@@ -0,0 +1,9 @@
+report = {
+    "your_name": "Assu",
+    "your_area": "Bangalore",
+    "issue_type": "Road Damage",
+    "photo_filename": "road.jpg"
+}
+
+print(report["your_name"])
+print(report["issue_type"])
