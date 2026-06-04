362bfa33cb4508fb5d318e3f8757e5dc68be08c9
mohamedasfaaqshahul-cell <mohamedasfaaq.shahul@gmail.com>
2026-06-04 16:42:22 +0530

    Create exersice5.py

---
+++ Day3/exersice5.py
@@ -0,0 +1,9 @@
+report = {
+    "your_name": "Mohamed",
+    "your_area": "Salem",
+    "issue_type": "Road Damage",
+    "photo_filename": "road.jpg"
+}
+
+for key, value in report.items():
+    print(key, ":", value)
