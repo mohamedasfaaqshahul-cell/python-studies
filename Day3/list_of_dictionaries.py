b029dd17250a56bc1da8b733aa4c3ae5590fc8b5
mohamedasfaaqshahul-cell <mohamedasfaaq.shahul@gmail.com>
2026-06-04 16:40:03 +0530

    Create list_of_dictionaries.py

---
+++ Day3/list_of_dictionaries.py
@@ -0,0 +1,8 @@
+reports = [
+    {"name": "Assu", "issue": "Garbage"},
+    {"name": "Mohamed", "issue": "Water Leakage"},
+    {"name": "Sahana", "issue": "Road Damage"}
+]
+
+for report in reports:
+    print(report)
